403c4bb0c632c42dcdc0b7d249278d51e1686198
sasonii <sasonshmuellameey@gmail.com>
2022-11-18 23:05:37 +0200

    q2-done

--- wet/hw1_wet_files/ex2.asm
+++ wet/hw1_wet_files/ex2.asm
@@ -2,4 +2,44 @@
 
 .section .text
 _start:
-#your code here
+    xor %rax, %rax
+    movl num, %eax
+    cdq
+    movl $0,%ecx    
+    cmp %ecx,%edx
+    je possitive_hw1
+    jne negative_hw1
+    
+possitive_hw1:
+    xor %rbx, %rbx
+    mov $source, %rdx
+    mov $destination, %r8 
+.Loop1_hw1:
+    movb (%rdx,%rbx), %cl
+    movb %cl, (%r8,%rbx,1)
+    lea (%r8,%rbx,1), %rcx
+    mov (%rcx), %rcx
+    
+    inc %rbx
+    dec %eax
+    test %eax, %eax
+    jne .Loop1_hw1
+    
+    jmp end_hw1
+
+negative_hw1:
+    imul $-1, %eax
+    xor %rbx, %rbx
+
+    mov $source, %rdx
+    mov $destination, %r8 
+.Loop2_hw1:
+    movb (%rdx,%rbx), %cl
+    movb %cl, -1(%r8,%rax,1)
+    
+    inc %rbx 
+    dec %eax 
+    test %eax, %eax
+    jne .Loop2_hw1
+    
+end_hw1:
