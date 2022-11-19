3207889e6fa1313546e7a5ff7f2c2f9009fe6632
sasonii <sasonshmuellameey@gmail.com>
2022-11-19 15:21:24 +0200

    q3_fix_bug

--- wet/hw1_wet_files/ex3.asm
+++ wet/hw1_wet_files/ex3.asm
@@ -10,8 +10,8 @@ _start:
     mov $mergedArray, %r11
     
 .Loop1_hw1:
-    #lea -4(%r8, %r11), %r13
-    #movl (%r13), %r13d
+    lea -4(%r8, %r11), %r13
+    movl (%r13), %r13d
     
     movl (%r9, %rdx), %ebx
     movl (%r10, %rax), %ecx
@@ -20,13 +20,19 @@ _start:
     jb lesser_hw1
     je equal_hw1
 
-greater_hw1:    
+greater_hw1:
+    movl -4(%r11, %r8), %r12d
+    cmpl %r12d,%ebx 
+    je not_equal_and_exist_greater_hw1
     movl %ebx, (%r8,%r11,1)    
     add $4,%rdx
     add $4,%r8
     jmp .Loop1_hw1
 
 lesser_hw1:
+    movl -4(%r11, %r8), %r12d
+    cmpl %r12d,%ecx 
+    je not_equal_and_exist_lesser_hw1
     movl %ecx, (%r8,%r11,1) 
     add $4,%rax
     add $4,%r8
@@ -36,11 +42,18 @@ equal_hw1:
     cmpl $0,%ebx
     je ending_hw1
     
-    movl (%r11, %r8), %r12d
+    movl -4(%r11, %r8), %r12d
     cmpl %r12d,%ebx
     je equal_and_exist_hw1
-    jne equal_and_exist_hw1
+    jne equal_and_not_exist_hw1
     
+not_equal_and_exist_greater_hw1:
+    add $4,%rdx
+    jmp .Loop1_hw1    
+    
+not_equal_and_exist_lesser_hw1:
+    add $4,%rax
+    jmp .Loop1_hw1
 
 equal_and_exist_hw1:
     add $4,%rax
@@ -52,6 +65,9 @@ equal_and_not_exist_hw1:
     add $4,%rdx
     add $4,%r8
     jmp .Loop1_hw1 
+    
 ending_hw1:
     movl $0 , (%r11, %r8)
+
+    
 
